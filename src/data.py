3c97a0e91887571e512fd8d212593d1f16613817
Brandon D <bdicicco@uwo.ca>
2019-02-15 14:56:07 -0500

    Initial data class and functions to set the data. ID system in-progress

--- src/data.py
+++ src/data.py
@@ -1,6 +1,24 @@
 import numpy as np
 
 
+class Data:
+    def __init__(self):
+        self.data = np.empty()
+        self.id = np.empty.__hash__()
+
+    def set_random(self, amount, minimum, maximum):
+        self.data = random_data(amount, minimum, maximum)
+        self.id = self.__hash__()
+
+    def set_linear(self, amount, slope, y_int):
+        self.data = linear_noise(amount, slope, y_int)
+        self.id = self.__hash__()
+
+    def set_data(self, data):
+        self.data = data
+        self.id = self.__hash__()
+
+        
 def random_data(amount, minimum, maximum):
     if minimum >= maximum:
             raise ValueError("Minimum must be less than maximum")
