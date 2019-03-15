c1701350c84cf5391ab8ad1bdd14f89472521478
Brandon D <bdicicco@uwo.ca>
2019-03-14 20:27:00 -0400

    Basic Bokeh test implementation for visualizations

---
+++ src/visualization.py
@@ -0,0 +1,28 @@
+import data
+
+from bokeh.plotting import figure, output_file, show
+
+# prepare some data
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
+
+# output to static HTML file
+output_file("lines.html")
+
+TOOLS = "hover"
+
+# create a new plot with a title and axis labels
+p = figure(tooltips=TOOLS, title="simple line example", x_axis_label='x', y_axis_label='y')
+
+# add a line renderer with legend and line thickness
+p.line(x, y, legend="Temp.", line_width=2)
+p.toolbar.logo = "grey"
+p.background_fill_color = "#dddddd"
+p.hover.tooltips = [
+    ("x", "@x"),
+    ("y", "@y")
+]
+
+# show the results
+show(p)
+
