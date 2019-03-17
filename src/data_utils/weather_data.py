4da9593c20d8905f5a4f25ce98f9eb1c6a193d28
Riddhi Sharma <rshar59@uwo.ca>
2019-03-16 17:48:26 -0700

    Added image data

--- src/data_utils/weather_data.py
+++ src/data_utils/weather_data.py
@@ -1,10 +1,23 @@
-
 import pandas as pd
-df = pd.read_csv("../../data/weatherHistory.csv",delimiter=',')
-df2=pd.read_csv("../../data/data_monthly_rainfall.csv",delimiter=',')
-# meanTemp=df['Temperature (C)'].mean()
-print("----------Szeged Hungary----------")
-print(df)
-print("----------Bangladesh----------")
-print(df2)
-# print('Mean temp: ' + str(meanTemp))
+
+
+def weatherData():
+    df = pd.read_csv("../../data/weatherHistory.csv", delimiter=',')
+    df2 = pd.read_csv("../../data/data_monthly_rainfall.csv", delimiter=',')
+    # meanTemp=df['Temperature (C)'].mean()
+
+    print("----------Szeged Hungary----------")
+    print(df)
+    print("----------Bangladesh----------")
+    print(df2)
+
+
+
+
+
+
+def main():
+    weatherData()
+
+if __name__ == '__main__':
+    main()
