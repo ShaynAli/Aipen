5cde48b147dde6b4d5b2af972427b0ca271bd09f
Brandon D <bdicicco@uwo.ca>
2019-03-18 22:20:41 -0400

    Initial play button functionality (no button itself yet)

--- src/server/assets/script.js
+++ src/server/assets/script.js
@@ -3,6 +3,7 @@ function activitySelect() {
     if (x == "Test") {
         document.getElementById("group1").disabled = false;
         document.getElementById("group2").disabled = true;
+        document.getElementById("dataSelect").style.visibility = "visible";
     }
 
     if (x == "Test2") {
@@ -11,3 +12,35 @@ function activitySelect() {
     }
 }
 
+function dataHide() {
+    document.getElementById("dataSelect").style.visibility = "hidden";
+    document.getElementById("group1").disabled = true;
+    document.getElementById("group2").disabled = true;
+}
+
+function play() {
+    var activity = document.getElementById("activity").value;
+    var data = document.getElementById("dataSelect").value;
+    var currentState = []
+
+    currentState.push(activity)
+    currentState.push(data)
+
+    if (document.getElementById("model1").checked) {
+        currentState.push("model1");
+    }
+
+    if (document.getElementById("model2").checked) {
+        currentState.push("model2");
+    }
+
+    if (document.getElementById("model3").checked) {
+        currentState.push("model3");
+    }
+
+    if (document.getElementById("model4").checked) {
+        currentState.push("model4");
+    }
+
+    
+}
